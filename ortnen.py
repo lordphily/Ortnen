241b7dd5f364d2311e431d7471036b2396221c7a
lordphily <philipp.kempert@web.de>
2019-04-01 22:42:48 +0200

    bessere latex und csv mit anhangstext

--- ortnen.py
+++ ortnen.py
@@ -8,34 +8,37 @@ Created on Fri Mar 29 21:32:05 2019
 
 import csv
  
-def build_latex(x,y):
+def build_latex(x):
     """
-    Mache aus x und y ein Latex Dokument
+    Mache aus x ein latex bereich des dokuments
+    
     """
     eingabe=[]
-    eingabe.append("\section{%s}" %(y[2]))
-    eingabe.append("\ \bbegin{tabular}{ll}")
-    eingabe.append(r"Abstimmendes Gremium: & %s\\" %(y[1]))
-    eingabe.append(r"Datum: & %s\\" %(y[0]))
-    eingabe.append(r"Anatrag/Beschluss wurde: & %s\\" %(y[9]))
-    y[11]=y[11].replace(" ","")
-    kw=y[11].split(",")
+    eingabe.append("\section{%s}" %(x[2]))
+    eingabe.append("\ \bbegin{tabularx}{\linewidth}{lX}")
+    eingabe.append(r"\textbf{Abstimmendes Gremium:} & %s\\" %(x[1]))
+    eingabe.append(r"\textbf{Datum:} & %s\\" %(x[0]))
+    eingabe.append(r"\textbf{Anatrag/Beschluss wurde} & %s\\" %(x[9]))
+    x[11]=x[11].replace(" ","")
+    kw=x[11].split(",")
     for i in range(0,len(kw)):
         if i==0:
-            eingabe.append(r"Keyword: & %s\\" %(kw[i]))
+            eingabe.append(r"\textbf{Keyword:} & %s\\" %(kw[i]))
         else:
             eingabe.append(r" & %s\\" %(kw[i]))
-    eingabe.append("\end{tabular}")
-    eingabe.append("\ \bbegin{tabular}{ccc}")
-    eingabe.append(r"\multicolumn{3}{c}{Abstimmungsergebniss}\\")
+    eingabe.append("\end{tabularx}")
+    eingabe.append("\ \bbegin{tabularx}{\linewidth}{XXX}")
+    eingabe.append(r"\multicolumn{3}{l}{\textbf{Abstimmungsergebniss:}}\\")
     eingabe.append(r"Zustimmung & Ablehnung & Enthaltungen \\")
-    eingabe.append(r"{} & {} & {} \\".format(y[6],y[7],y[8]))
-    eingabe.append("\end{tabular}")
+    eingabe.append(r"{} & {} & {} \\".format(x[6],x[7],x[8]))
+    eingabe.append("\end{tabularx}")
     eingabe.append("\subsection*{Antragstext}")
-    eingabe.append(y[3])
+    eingabe.append(x[3])
     eingabe.append("\subsection*{Begründung}")
-    eingabe.append(y[4])
-
+    eingabe.append(x[4])
+    if x[10]!="":
+        eingabe.append("\subsection{Anhang}")
+        eingabe.append(x[10])
     
     ausgabe=""
     for i in range(0,len(eingabe)):
@@ -45,12 +48,12 @@ def build_latex(x,y):
     
 x,y=[],[]
 
-with open('Beschluss01.csv',) as f:
+with open('Beschluss02.csv',) as f:
     reader = csv.reader(f, delimiter=';')
     for row in reader:
         x.append(row[0])
         y.append(row[1])
 
 c=y[11].split(",")
-a=build_latex(x,y)
+a=build_latex(y)
 print(a)
